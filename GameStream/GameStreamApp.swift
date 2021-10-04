425de3bf7f39b6fea6cc0d8a0f8df12e40497c60
Moises Morales <moisesmorales1995@gmail.com>
2021-10-03 19:09:17 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Moisés on 03/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
