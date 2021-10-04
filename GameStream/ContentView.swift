d8c5b0a53672522297cac258267065ca1c8abc8e
Moises Morales <moisesmorales1995@gmail.com>
2021-10-03 23:17:35 -0500

    Escribiendo la lógica para mostrar dos pantallas

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Mi segundo commit!")
-            .padding()
+        ZStack {
+            Color(red: 0.07, green: 0.12, blue: 0.24).ignoresSafeArea()
+            VStack{
+                Image("applogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250)
+                    .padding(.bottom, 42)
+                InicioRegistroView()
+            }
+            
+        }
+    }
+}
+
+struct InicioRegistroView:View{
+    
+    @State var tipoInicioSesion = true
+    
+    var body: some View{
+        VStack{
+            HStack {
+                Spacer()
+                Button("INICIA SESIÓN"){
+                    tipoInicioSesion = true
+                    print("Pantalla inicio sesión")
+                }.foregroundColor( tipoInicioSesion ? .white: .gray)
+                Spacer()
+                Button("REGÍSTRATE"){
+                    tipoInicioSesion = false
+                    print("Pantalla de registro")
+                }.foregroundColor( tipoInicioSesion ? .gray: .white)
+                Spacer()
+            }
+            Spacer(minLength: 42)
+            if tipoInicioSesion == true {
+                InicioView()
+            }else{
+                RegistroView()
+            }
+        }
+    }
+}
+
+struct InicioView:View{
+    var body: some View {
+        Text("Soy la vista de inicio de sesión")
+    }
+}
+struct RegistroView:View{
+    var body: some View {
+        Text("Soy la vista de registro")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        Image("pantalla01")
     }
 }
