5da7445bca59d60fa191959ce35d40d81ae4370c
Moises Morales <moisesmorales1995@gmail.com>
2021-10-04 01:42:19 -0500

    Terminando la pantalla de inicio de sesion

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -54,10 +54,85 @@ struct InicioRegistroView:View{
 }
 
 struct InicioView:View{
+    
+    @State var correo = ""
+    @State var contrasena = ""
+    
     var body: some View {
-        Text("Soy la vista de inicio de sesión")
+        ScrollView{
+            VStack(alignment: .leading){
+                
+                //Campo Correo Electronico
+                Text("Correo electrónico").bold()
+                ZStack(alignment:.leading){
+                    if correo.isEmpty{
+                        Text("Escribe tú correo electronico").font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    TextField("", text: $correo)
+                }
+                Divider().frame( height: 1)
+                    .background(Color("dark-cian"))
+                    .padding(.bottom)
+                
+                //Campo Contraseña
+                Text("Contraseña").bold()
+                    .foregroundColor(Color("light-gray"))
+                
+                ZStack(alignment:.leading){
+                    if contrasena.isEmpty{
+                        Text("Escribe tú contraseña aquí").font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    SecureField("", text: $contrasena)
+                }
+                Divider().frame( height: 1)
+                    .background(Color("dark-cian"))
+                    .padding(.bottom)
+                
+                Text("¿Olvidaste tu contraseña?").font(.footnote)
+                    .frame(width: 300, alignment: .trailing)
+                    .padding(.bottom)
+                
+                Button(action: iniciarSesion, label: {
+                    Text("INICIAR SESÍON").fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6.0).stroke(lineWidth: 2.0).shadow(color: .white, radius: 6))
+                    
+                })
+                
+                Text("Inicia sesión con redes sociales").foregroundColor(.white)
+                    .frame(width: 300, alignment: .center)
+                    .padding(EdgeInsets(top: 40, leading: 0, bottom: 10, trailing: 0))
+                HStack{
+                    Button(action: {print("iniciar facebook")}, label: {
+                        Text("Facebook").fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 40, alignment: .center)
+                            .background(Color(.white).opacity(0.1))
+                    }).cornerRadius(8.0)
+                    
+                    Button(action: {print("iniciar twitter")}, label: {
+                        Text("Twitter").fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 40, alignment: .center)
+                            .background(Color(.white).opacity(0.1))
+                    }).cornerRadius(8.0)
+                }.foregroundColor(.white)
+                
+            }.foregroundColor(Color("dark-cian"))
+                .padding()
+            
+        }.padding(.horizontal,77.0)
     }
 }
+
+func iniciarSesion(){
+    print("Estoy iniciando sesión")
+}
+
 struct RegistroView:View{
     var body: some View {
         Text("Soy la vista de registro")
